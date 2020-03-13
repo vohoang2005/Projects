ab2e9098279f0fea8466a84f2ce6ac778d5c2cee
Chuong H Vo <vohoang2005@gmail.com>
2020-03-13 16:25:06 -0700

    Add files via upload

---
+++ room.rb
@@ -0,0 +1,36 @@
+class Room
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+    def capacity
+        @capacity
+    end
+
+    def occupants
+        @occupants
+    end
+
+    def full?
+        if @occupants.length < @capacity
+            return false
+
+        else 
+            true
+        end
+    end
+
+    def available_space
+        @capacity - @occupants.length
+    end
+
+    def add_occupant(person)
+        if self.full?
+            return false
+        else
+            @occupants << person
+            return true
+        end
+    end
+end
